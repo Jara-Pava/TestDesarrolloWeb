5bff66816d36632c80b4991dad84aa7c8cd11b32
JaraPava <jose.jara@grupo-giga.com>
2026-02-26 09:01:54 -0600

    centralizar stored-procedure config & executors y obtener usuarios

--- DataAccessDesarrollos/StoreProcedureCollection.cs
+++ DataAccessDesarrollos/StoreProcedureCollection.cs
@@ -1,41 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessDesarrollos
 {
     public class StoreProcedureCollection : ConfigurationElementCollection
     {
-        public StoreProcedureCollection()
-        {
-        }
-
-        public StoreProcedure this[int index]
-        {
-            get { return (StoreProcedure)BaseGet(index); }
-            set
-            {
-                if (BaseGet(index) != null)
-                {
-                    BaseRemoveAt(index);
-                }
-                BaseAdd(index, value);
-            }
-        }
-
-        public void Add(StoreProcedure depConfig)
-        {
-            BaseAdd(depConfig);
-        }
-
-        public void Clear()
-        {
-            BaseClear();
-        }
-
         protected override ConfigurationElement CreateNewElement()
         {
             return new StoreProcedure();
@@ -46,19 +15,18 @@ namespace DataAccessDesarrollos
             return ((StoreProcedure)element).Code;
         }
 
-        public void Remove(StoreProcedure depConfig)
+        public StoreProcedure this[int index] => (StoreProcedure)BaseGet(index);
+
+        public new StoreProcedure this[string code] => (StoreProcedure)BaseGet(code);
+
+        public void Add(StoreProcedure sp) => BaseAdd(sp);
+
+        public void Remove(string code)
         {
-            BaseRemove(depConfig.Code);
+            if (BaseGet(code) != null)
+                BaseRemove(code);
         }
 
-        public void RemoveAt(int index)
-        {
-            BaseRemoveAt(index);
-        }
-
-        public void Remove(string name)
-        {
-            BaseRemove(name);
-        }
+        public void Clear() => BaseClear();
     }
 }
